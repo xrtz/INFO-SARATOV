a212f8534c2d7dd52b0ee0246aa4bbd8fe7b90b8
Ksardass <and_melnikov2003@mail.ru>
2020-05-12 01:01:18 +0400

    Ничего не работет

--- p.py
+++ p.py
@@ -1,3 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+
 def parsing(coords1, coords2, type):
     url = 'https://yandex.ru/maps/?'
     params = {'ll': '37.833217,55.659301',
@@ -9,14 +13,9 @@ def parsing(coords1, coords2, type):
     try:
         response = requests.get(url, params=params).text
         soup = BeautifulSoup(response, 'html.parser')
-        ans = soup.find('div', class_='masstransit-route-snippet-view__route-duration')
-        ans = ans[ans[:-1].rfind('>') + 1:-6]
-        time = 0
-        if 'ч' in ans:
-            time += 60 * int(ans[:ans.find('ч') - 1])
-        if 'мин' in ans:
-            time += int(ans[max(0, ans.find('мин') - 3):ans.find('мин') - 1])
-        return time
+        ans1 = str(soup.find('div', class_='auto-route-snippet-view__route-title-primary'))
+        ans = ans1[ans1[:-1].rfind('>') + 1:-6]
+        return ans
     except Exception:
         if response:
             print('Ошибка нахождения пути')
